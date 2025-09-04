0bd1d92bf8812c9705f432bb0d087c8a44f05484
michaelj <michaelj@ie.ibm.com>
2025-09-04 10:24:49 +0100

    feat: Reraise AttributeError as ValueError
    This is more appropriate and is also handled as expected by pydantic in validation test (causes a ValidationError)

--- orchestrator/modules/module.py
+++ orchestrator/modules/module.py
@@ -159,7 +159,14 @@ def load_module_class_or_function(conf: ModuleConf):
     Returns:
         The module class or function
 
+    Raises: ValueError if no class/function found matching conf
+
     """
 
     attribute = conf.moduleClass if conf.moduleClass else conf.moduleFunction
-    return getattr(load_module(conf), attribute)
+    try:
+        retval = getattr(load_module(conf), attribute)
+    except AttributeError as error:
+        raise ValueError(f"Unable to load class or function from {conf}") from error
+
+    return retval

07a6e60f4808ba60f996f67ac0d3052c634b1fba
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2025-09-16 16:58:04 +0100

    feat: keep reference to queue task

--- orchestrator/api/rest.py
+++ orchestrator/api/rest.py
@@ -22,7 +22,10 @@ class AdoRESTApi:
 
     def __init__(self):
         configure_logging()
-        asyncio.get_event_loop().create_task(watch_queue())
+
+        # AP 16/09/2025: keep reference to the task
+        # due to https://github.com/python/cpython/issues/88831
+        self.task_reference = asyncio.get_event_loop().create_task(watch_queue())
 
 
 ado_rest_api = AdoRESTApi.bind()

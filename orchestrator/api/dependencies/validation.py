2244023600543f8a6731d869ae2f3df76d2740ab
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2025-09-18 16:22:42 +0100

    feat: add validation on entities submitted for experiment

--- orchestrator/api/dependencies/validation.py
+++ orchestrator/api/dependencies/validation.py
@@ -1,11 +1,15 @@
 # Copyright (c) IBM Corporation
 # SPDX-License-Identifier: MIT
-
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
 
-from orchestrator.modules.actuators.registry import ActuatorRegistry
+from orchestrator.modules.actuators.registry import (
+    ActuatorRegistry,
+    UnknownExperimentError,
+)
+from orchestrator.schema.entity import Entity
+from orchestrator.schema.reference import ExperimentReference
 
 
 def validated_actuator_id(actuator_id: str) -> str:
@@ -21,15 +25,35 @@ def validated_actuator_id(actuator_id: str) -> str:
 def validated_experiment_id(
     actuator_id: Annotated[str, Depends(validated_actuator_id)], experiment_id: str
 ) -> str:
-    actuator = ActuatorRegistry().actuatorForIdentifier(actuatorid=actuator_id)
-    available_experiments_for_actuator = {
-        experiment.identifier for experiment in actuator.catalog().experiments
-    }
-
-    if experiment_id not in available_experiments_for_actuator:
+    try:
+        ActuatorRegistry().experimentForReference(
+            ExperimentReference(
+                experimentIdentifier=experiment_id, actuatorIdentifier=actuator_id
+            )
+        )
+    except UnknownExperimentError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Actuator {actuator_id} does not have experiment {experiment_id}",
         )
 
     return experiment_id
+
+
+def entity_is_valid_for_experiment(
+    actuator_id: str, experiment_id: str, entities: list[Entity]
+):
+    requested_experiment = ActuatorRegistry().experimentForReference(
+        ExperimentReference(
+            experimentIdentifier=experiment_id, actuatorIdentifier=actuator_id
+        )
+    )
+
+    try:
+        for entity in entities:
+            requested_experiment.propertyValuesFromEntity(entity)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=str(e),
+        )

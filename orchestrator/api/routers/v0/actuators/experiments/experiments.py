ed34869d36640b7a25d094827ccbaf22e8cf28d7
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2025-09-18 16:23:22 +0100

    refactor: simplify code

--- orchestrator/api/routers/v0/actuators/experiments/experiments.py
+++ orchestrator/api/routers/v0/actuators/experiments/experiments.py
@@ -1,12 +1,16 @@
 # Copyright (c) IBM Corporation
 # SPDX-License-Identifier: MIT
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 
-from orchestrator.api.dependencies.validation import validated_actuator_id
+from orchestrator.api.dependencies.validation import (
+    validated_actuator_id,
+    validated_experiment_id,
+)
 from orchestrator.api.routers.v0.actuators.experiments.requests import requests
 from orchestrator.modules.actuators.registry import ActuatorRegistry
 from orchestrator.schema.experiment import Experiment
+from orchestrator.schema.reference import ExperimentReference
 
 router = APIRouter(
     prefix="/{actuator_id}/experiments",
@@ -28,22 +32,12 @@ async def list_actuator_experiments(actuator_id: str) -> list[Experiment]:
     )
 
 
-@router.get(
-    "/{experiment_id}",
-)
+@router.get("/{experiment_id}", dependencies=[Depends(validated_experiment_id)])
 async def get_actuator_experiment_by_id(
     actuator_id: str, experiment_id: str
 ) -> Experiment:
-    actuator = ActuatorRegistry().actuatorForIdentifier(actuatorid=actuator_id)
-    identifier_experiment_map = {
-        experiment.identifier: experiment
-        for experiment in actuator.catalog().experiments
-    }
-
-    if experiment_id not in identifier_experiment_map:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Actuator {actuator_id} does not have experiment {experiment_id}",
+    return ActuatorRegistry().experimentForReference(
+        ExperimentReference(
+            experimentIdentifier=experiment_id, actuatorIdentifier=actuator_id
         )
-
-    return identifier_experiment_map[experiment_id]
+    )

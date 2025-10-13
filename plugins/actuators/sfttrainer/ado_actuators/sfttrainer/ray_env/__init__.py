1a1894c987fad95fdd20ac9c90aaac183e5f6ba2
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2025-10-13 14:51:27 +0100

    feat: implement a RuntimeEnvPlugin to guide installation order of packages
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

---
+++ plugins/actuators/sfttrainer/ado_actuators/sfttrainer/ray_env/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) IBM Corporation
+# SPDX-License-Identifier: MIT
+
+import ado_actuators.sfttrainer.ray_env.ordered_pip as ordered_pip  # noqa: F401
+import ado_actuators.sfttrainer.ray_env.utils as utils  # noqa: F401
---
+++ plugins/actuators/sfttrainer/ado_actuators/sfttrainer/tests/test_env_plugin.py
@@ -0,0 +1,99 @@
+import os
+
+import ado_actuators.sfttrainer.ray_env as ray_env
+import pytest
+import ray
+
+
+@pytest.fixture
+def set_plugin():
+    os.environ["RAY_RUNTIME_ENV_PLUGINS"] = (
+        '[{"class":"' + ray_env.ordered_pip.OrderedPipPlugin.ClassPath + '"}]'
+    )
+
+    yield
+
+    del os.environ["RAY_RUNTIME_ENV_PLUGINS"]
+
+
+def test_ray_runtime_env_with_ordered_pip_plugin(set_plugin):
+    class_path = ".".join(
+        (
+            ray_env.ordered_pip.OrderedPipPlugin.__module__,
+            ray_env.ordered_pip.OrderedPipPlugin.__name__,
+        )
+    )
+
+    assert class_path == ray_env.ordered_pip.OrderedPipPlugin.ClassPath
+
+    if not ray_env.utils.is_ordered_pip_available():
+        pytest.skip("ordered_pip is unavailable")
+
+    packages = ["torch==2.6.0", "flash_attn==2.7.4.post1", "mamba-ssm==2.2.5"]
+
+    runtime_env = ray_env.utils.get_ray_environment(
+        packages=packages,
+        packages_requiring_extra_phase=[["flash_attn", "mamba-ssm"]],
+    )
+
+    assert runtime_env == {
+        "env_vars": {"AIM_UI_TELEMETRY_ENABLED": "0", "PIP_NO_BUILD_ISOLATION": "0"},
+        "ordered_pip": {
+            "phases": [
+                {"packages": ["torch==2.6.0"]},
+                {"packages": ["flash_attn==2.7.4.post1", "mamba-ssm==2.2.5"]},
+            ]
+        },
+    }
+
+
+def test_ray_runtime_env_with_vanilla_pip():
+    if not ray_env.utils.is_pip_available():
+        pytest.skip("pip is unavailable")
+
+    assert ray_env.utils.is_ordered_pip_available() is False
+
+    packages = ["torch==2.6.0", "flash_attn==2.7.4.post1", "mamba-ssm==2.2.5"]
+
+    runtime_env = ray_env.utils.get_ray_environment(
+        packages=packages,
+        packages_requiring_extra_phase=[["flash_attn", "mamba-ssm"]],
+    )
+
+    assert runtime_env == {
+        "env_vars": {"AIM_UI_TELEMETRY_ENABLED": "0", "PIP_NO_BUILD_ISOLATION": "0"},
+        "pip": {
+            "packages": ["torch==2.6.0", "flash_attn==2.7.4.post1", "mamba-ssm==2.2.5"]
+        },
+    }
+
+
+def test_ordered_pip_plugin(set_plugin):
+    if not ray_env.utils.is_nvidia_smi_available():
+        pytest.skip("there's no NVIDIA gpu on this machine")
+
+    @ray.remote(
+        runtime_env={
+            "ordered_pip": {
+                "phases": [
+                    ["torch==2.6.0"],
+                    {
+                        "packages": ["mamba-ssm==2.2.5"],
+                        "pip_install_options": ["--no-build-isolation"],
+                    },
+                ]
+            },
+            "env_vars": {
+                "LOG_LEVEL": "debug",
+                "LOGLEVEL": "debug",
+            },
+        },
+    )
+    def try_import_torch():
+        import torch
+
+        print(torch.__file__)
+        assert torch.__version__ == "2.6.0"
+        return True
+
+    assert ray.get(try_import_torch.remote())

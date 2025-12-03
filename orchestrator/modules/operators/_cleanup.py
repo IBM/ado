627f4ecb7e7c96bbc2231e79f061cf51d7ec5ff3
michaelj <michaelj@ie.ibm.com>
2025-12-03 16:48:25 +0000

    refactor(core): operation cleanup
    Operation cleanup logic was mixed together (a single function for cleaning up everything was required/was different for different scenarios) and also mixed with signal handling (a shutdown could be successful operation or a signal).
    
    In this commit
    - cleanup code required by different operations/functions is separated
    - A single signal handler exists that is generic
       - operations register their cleanup requirements with the handler so they are cleaned-up
    - shutdown global now only indicates if a signal was received NOT if an operation finished

--- orchestrator/modules/operators/_cleanup.py
+++ orchestrator/modules/operators/_cleanup.py
@@ -10,10 +10,43 @@ import ray
 from ray.actor import ActorHandle
 
 shutdown_signal_received = False
+shutdown_signal_received = False
 CLEANER_ACTOR = "resource_cleaner"
 
 moduleLog = logging.getLogger("orchestration_cleanup")
 signal_cleanup_callbacks: dict[str, Callable[[], None]] = OrderedDict()
+
+
+def graceful_operation_shutdown_signal_handler() -> (
+    typing.Callable[[int, typing.Any | None], None]
+):
+    """Handler which executes cleanup callbacks registered by operations on receiving a signal"""
+
+    def handler(sig, frame):
+
+        moduleLog.warning(f"Got signal {sig}")
+        global shutdown_signal_received
+        global signal_cleanup_callbacks
+
+        if not shutdown_signal_received:
+            shutdown_signal_received = True
+            moduleLog.debug("Cleanup custom actors")
+            try:
+                cleaner_handle = ray.get_actor(name=CLEANER_ACTOR)
+                ray.get(cleaner_handle.cleanup.remote())
+                # deleting a cleaner actor. It is detached one, so has to be deleted explicitly
+                ray.kill(cleaner_handle)
+            except ValueError:
+                moduleLog.debug("No ray actor")
+            except Exception as e:
+                moduleLog.warning(f"Failed to cleanup custom actors {e}")
+
+            moduleLog.warning("Calling cleanup callbacks")
+            for subscriber in signal_cleanup_callbacks:
+                moduleLog.warning(f"Cleaning {subscriber}")
+                signal_cleanup_callbacks[subscriber]()
+        else:
+            moduleLog.info("Graceful shutdown already completed")
 
 
 def graceful_operation_shutdown_signal_handler() -> (

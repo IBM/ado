a9b0514f4e2a29dd1c246c1a31bfb526f26e5b2b
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2026-01-09 13:41:19 +0000

    chore: ignore S311 where not needed
    Standard pseudo-random generators are not suitable for cryptographic purposes

--- orchestrator/utilities/naming.py
+++ orchestrator/utilities/naming.py
@@ -8,6 +8,6 @@ import string
 def get_random_name_extension(length=5):
     alphabet = string.ascii_lowercase + string.ascii_uppercase
     return "".join(
-        random.choice(alphabet)
-        for i in range(length)  # noqa: S311 - not cryptographic purposes
+        random.choice(alphabet)  # noqa: S311 - not cryptographic purposes
+        for i in range(length)
     )

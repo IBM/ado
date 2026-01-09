7d09611cfd42f8412a899c98ea1e9deacdbabc17
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2026-01-09 11:06:46 +0000

    chore: ignore trng requirement for non-crypto purposes

--- orchestrator/utilities/naming.py
+++ orchestrator/utilities/naming.py
@@ -7,4 +7,6 @@ import string
 
 def get_random_name_extension(length=5):
     alphabet = string.ascii_lowercase + string.ascii_uppercase
-    return "".join(random.choice(alphabet) for i in range(length))
+    return "".join(
+        random.choice(alphabet) for i in range(length)
+    )  # noqa: S311 - not cryptographic purposes

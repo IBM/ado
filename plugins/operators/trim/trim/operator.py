88a799ef2270fc977fbddc2235874452064f86eb
Daniele Lotito <daniele.lotito@ibm.com>
2026-01-16 13:02:00 +0000

    docs: improve logging message

--- plugins/operators/trim/trim/operator.py
+++ plugins/operators/trim/trim/operator.py
@@ -80,10 +80,10 @@ def trim(
         len(source_df) < params.samplingBudget.minPoints
     ) and current_iter < max_iter:
         logger_trim.warning(
-            f"""Only {len(source_df)} points in the source space.
-                Starting with no-prior characterization for {params.samplingBudget.minPoints - len(source_df)}
-                Note: Trim sampler has been called with a minimum budget of {params.samplingBudget.minPoints} points.
-            """
+            f"Only {len(source_df)} points in the source space.\n"
+            "Starting with no-prior characterization operation, "
+            f"it will sample {params.samplingBudget.minPoints - len(source_df)} points.\n"
+            f"Note: Trim sampler has been called with a minimum budget of {params.samplingBudget.minPoints} points."
         )
 
         no_priors_params = params.noPriorParameters

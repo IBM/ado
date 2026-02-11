531c64448671454f333b3b4267c1b23bc9a28ca3
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2026-02-11 16:06:37 +0000

    refactor(cli): update ado get actuators

--- orchestrator/cli/resources/actuator/get.py
+++ orchestrator/cli/resources/actuator/get.py
@@ -1,6 +1,7 @@
 # Copyright IBM Corporation 2025, 2026
 # SPDX-License-Identifier: MIT
 
+import rich.box
 import typer
 from rich.status import Status
 
@@ -8,6 +9,7 @@ from orchestrator.cli.models.parameters import AdoGetCommandParameters
 from orchestrator.cli.models.types import AdoGetSupportedOutputFormats
 from orchestrator.cli.utils.output.prints import (
     ADO_INFO_EMPTY_DATAFRAME,
+    ADO_SPINNER_GETTING_OUTPUT_READY,
     ADO_SPINNER_INITIALIZING_ACTUATOR_REGISTRY,
     ERROR,
     HINT,
@@ -15,6 +17,7 @@ from orchestrator.cli.utils.output.prints import (
     WARN,
     console_print,
 )
+from orchestrator.utilities.rich import dataframe_to_rich_table
 
 
 def get_actuator(parameters: AdoGetCommandParameters) -> None:
@@ -30,73 +33,70 @@ def get_actuator(parameters: AdoGetCommandParameters) -> None:
     import orchestrator.modules.actuators
     import orchestrator.modules.actuators.registry
 
-    with Status(ADO_SPINNER_INITIALIZING_ACTUATOR_REGISTRY):
+    with Status(ADO_SPINNER_INITIALIZING_ACTUATOR_REGISTRY) as spinner:
         registry = (
             orchestrator.modules.actuators.registry.ActuatorRegistry.globalRegistry()
         )
 
-    if (
-        parameters.resource_id
-        and parameters.resource_id not in registry.actuatorIdentifierMap
-    ):
-        console_print(
-            f"{ERROR}Actuator {parameters.resource_id} does not exist.\n"
-            f"{HINT}Available actuators are: {list(registry.actuatorIdentifierMap.keys())}",
-            stderr=True,
-        )
-        raise typer.Exit(1)
+        if (
+            parameters.resource_id
+            and parameters.resource_id not in registry.actuatorIdentifierMap
+        ):
+            spinner.stop()
+            console_print(
+                f"{ERROR}Actuator {parameters.resource_id} does not exist.\n"
+                f"{HINT}Available actuators are: {list(registry.actuatorIdentifierMap.keys())}",
+                stderr=True,
+            )
+            raise typer.Exit(1)
 
-    if parameters.output_format != AdoGetSupportedOutputFormats.DEFAULT:
-        console_print(
-            f"{ERROR}Only the {AdoGetSupportedOutputFormats.DEFAULT.value} output format "
-            "is supported by this command.",
-            stderr=True,
-        )
-        raise typer.Exit(1)
+        if parameters.output_format != AdoGetSupportedOutputFormats.DEFAULT:
+            spinner.stop()
+            console_print(
+                f"{ERROR}Only the {AdoGetSupportedOutputFormats.DEFAULT.value} output format "
+                "is supported by this command.",
+                stderr=True,
+            )
+            raise typer.Exit(1)
 
-    if not parameters.show_details:
-        data = list(registry.actuatorIdentifierMap.keys())
-        columns = ["ACTUATOR ID"]
-    else:
+        spinner.update(ADO_SPINNER_GETTING_OUTPUT_READY)
+
         data = []
-        columns = ["ACTUATOR ID", "CATALOG ID", "EXPERIMENT ID", "SUPPORTED"]
+        columns = ["ACTUATOR ID", "EXPERIMENTS"]
 
         if parameters.resource_id:
             actuator_identifiers = [parameters.resource_id]
         else:
             actuator_identifiers = registry.actuatorIdentifierMap.keys()
 
-        for actuator_id in actuator_identifiers:
+        for actuator_id in sorted(actuator_identifiers):
             catalog = registry.catalogForActuatorIdentifier(actuator_id)
-            if not catalog.experiments:
+
+            # Count all experiments
+            total_experiments = len(catalog.experiments)
+
+            # Skip actuators with no experiments to show
+            if total_experiments == 0:
+                spinner.stop()
                 console_print(
                     f"{INFO}Actuator {actuator_id} has been omitted as it does not provide any experiment.",
                     stderr=True,
                 )
+                spinner.start()
                 continue
 
-            data.extend(
-                [
-                    actuator_id,
-                    catalog.identifier,
-                    experiment.identifier,
-                    not experiment.deprecated,
-                ]
-                for experiment in catalog.experiments
-                if not experiment.deprecated or parameters.show_deprecated
-            )
+            data.append([actuator_id, total_experiments])
 
-    output_df = pd.DataFrame(
-        data=data,
-        columns=columns,
+        output_df = pd.DataFrame(
+            data=data,
+            columns=columns,
+        )
+
+        if output_df.empty:
+            spinner.stop()
+            console_print(ADO_INFO_EMPTY_DATAFRAME, stderr=True)
+            return
+
+    console_print(
+        dataframe_to_rich_table(output_df, box=rich.box.SQUARE, show_edge=True)
     )
-
-    if parameters.resource_id:
-        output_df = output_df[output_df["ACTUATOR ID"] == parameters.resource_id]
-        output_df = output_df.reset_index(drop=True)
-
-    if output_df.empty:
-        console_print(ADO_INFO_EMPTY_DATAFRAME, stderr=True)
-        return
-
-    console_print(output_df, has_pandas_content=True)

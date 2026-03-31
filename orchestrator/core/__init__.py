8f5dc93427aecaee46e1fc49c8c30c53432d75b5
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2026-03-31 09:04:47 +0100

    fix(core): re-export AdoResource
    Signed-off-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>

--- orchestrator/core/__init__.py
+++ orchestrator/core/__init__.py
@@ -5,6 +5,7 @@ from .actuatorconfiguration.resource import ActuatorConfigurationResource
 from .datacontainer.resource import DataContainerResource
 from .discoveryspace.resource import DiscoverySpaceResource
 from .operation.resource import OperationResource
+from .resources import ADOResource as ADOResource
 from .resources import CoreResourceKinds
 from .samplestore.resource import SampleStoreResource
 

001af7a957ee74767266e74ea0bf709e069df8ec
mgazz <michele.gazzetti1@ibm.com>
2026-04-29 16:34:47 +0100

    feat(vllm_performance): add OpenTelemetry traces support (#920)
    * inject otel endpoint
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * update otel trace endpoint injection
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * test: add unit tests for otel endpoint
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * docs: add documentation on how to set the otel endpoint
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * fix: update env check
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * chore(vllm_performance): remove unrelated changes
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * refactor(vllm_performance): rename traces endpoint from otel to otlp
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * refactor(vllm_performance): set otlp traces endpoint type to pydantic.AnyUrl and propagate it through the entire chain
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * refactor(vllm_performance): update name refs and remove unnecessary env var
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * docs(vllm_performance): update names refs
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * feat(vllm_performance): set service name for otel traces
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * docs: udpate with otel service name
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Alessandro Pomponio <10339005+AlessandroPomponio@users.noreply.github.com>
    Signed-off-by: mgazz <michele.gazzetti1@ibm.com>
    
    * refactor(tests): instantiate models directly in test_vllm_otlp_traces
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    * test: add vllm_performance tests to tox.ini
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    
    ---------
    
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>
    Signed-off-by: mgazz <michele.gazzetti1@ibm.com>
    Co-authored-by: Alessandro Pomponio <10339005+AlessandroPomponio@users.noreply.github.com>

--- plugins/actuators/vllm_performance/ado_actuators/vllm_performance/env_manager.py
+++ plugins/actuators/vllm_performance/ado_actuators/vllm_performance/env_manager.py
@@ -112,6 +112,7 @@ class EnvironmentManager:
         verify_ssl: bool = False,
         pvc_name: str | None = None,
         pvc_template: str | None = None,
+        otlp_traces_endpoint: pydantic.AnyUrl | None = None,
     ) -> None:
         """
         Initialize
@@ -121,6 +122,7 @@ class EnvironmentManager:
         :param verify_ssl: flag verify SSL
         :param pvc_name: name of the PVC to be created / used
         :param pvc_template: template of the PVC to be created
+        :param otlp_traces_endpoint: OpenTelemetry traces endpoint URL
         """
         self.in_use_environments: dict[str, Environment] = {}
         self.free_environments: list[Environment] = []
@@ -130,6 +132,7 @@ class EnvironmentManager:
         self.max_concurrent = max_concurrent
         self.in_cluster = in_cluster
         self.verify_ssl = verify_ssl
+        self.otlp_traces_endpoint = otlp_traces_endpoint
 
         # component manager for cleanup
         self.manager = ComponentsManager(
@@ -297,6 +300,13 @@ class EnvironmentManager:
         await self.deployment_conflict_manager.wait(
             request_id=request_id, k8s_name=env.k8s_name, model=env.model
         )
+
+    def get_otlp_traces_endpoint(self) -> pydantic.AnyUrl | None:
+        """
+        Get the OTLP traces endpoint
+        :return: OTLP traces endpoint URL or None
+        """
+        return self.otlp_traces_endpoint
 
     def done_using(self, identifier: str, reclaim_on_completion: bool = False) -> None:
         """

7545e24c55f43f4233a242058d3fc1f566ffdbbc
Srikumar Venugopal <srikumar003@users.noreply.github.com>
2026-05-13 12:03:14 +0100

    fix(autoconf): Error in mapping Llama3.1-8b (#946)
    * fix(autoconf): Error in mapping Llama3.1-8b
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix(autoconf): error in base patter
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    ---------
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- plugins/custom_experiments/autoconf/autoconf/utils/config_mapper.py
+++ plugins/custom_experiments/autoconf/autoconf/utils/config_mapper.py
@@ -24,7 +24,7 @@ model_patterns = {
 mapped_models = {
     "GRANITE_3_1_2B": "granite-3.1-2b",
     "GRANITE_3_1_8B": "granite-3.1-8b-instruct",
-    "LLAMA_3_1_8B": "llama-3.1-8b",
+    "LLAMA_3_1_8B": "llama3.1-8b",
     "GRANITE_4_SMALL": "granite-4.0-h-small",
     "GRANITE_4_TINY": "granite-4.0-h-tiny",
     "GRANITE_4_MICRO": "granite-4.0-h-micro",

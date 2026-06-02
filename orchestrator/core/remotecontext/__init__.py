221d9b0b873a13f848007a074277eb52520ace01
michaelj <michaelj@ie.ibm.com>
2026-06-02 10:36:55 +0100

    feat(remote_execution): Allow setting params related to job env creation
    If you are expecting many jobs to start simultaneously with autoscaling it can be useful to changes these from the ray defaults.

--- orchestrator/core/remotecontext/__init__.py
+++ orchestrator/core/remotecontext/__init__.py
@@ -7,6 +7,7 @@ from .config import (
     PackageConfiguration,
     PortForwardConfiguration,
     RemoteExecutionContext,
+    RuntimeEnvironmentConfiguration,
 )
 
 __all__ = [
@@ -15,4 +16,5 @@ __all__ = [
     "PackageConfiguration",
     "PortForwardConfiguration",
     "RemoteExecutionContext",
+    "RuntimeEnvironmentConfiguration",
 ]

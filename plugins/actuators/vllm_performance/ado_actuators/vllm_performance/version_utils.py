aae73531f36845f88834f06a2eac1f9a20f0ccb4
mgazz <michele.gazzetti1@ibm.com>
2026-06-16 15:08:49 +0100

    Update plugins/actuators/vllm_performance/ado_actuators/vllm_performance/version_utils.py
    Co-authored-by: Christian Pinto <55737893+christian-pinto@users.noreply.github.com>
    Signed-off-by: mgazz <michele.gazzetti1@ibm.com>

--- plugins/actuators/vllm_performance/ado_actuators/vllm_performance/version_utils.py
+++ plugins/actuators/vllm_performance/ado_actuators/vllm_performance/version_utils.py
@@ -12,7 +12,7 @@ class VLLMVersionChecker:
     THREADPOOL_MIN_VERSION = "0.20.0"
 
     @classmethod
-    def extract_version_from_image(cls, image: str) -> str:
+    def extract_version_from_image(cls, image: str) -> str | None:
         """
         Extract version string from a container image string.
 

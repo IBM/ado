8fa3d06c52f9c476248f498a9fd89f90b158ac0b
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2026-07-08 20:45:33 +0100

    build: require ado 2 in plugins
    Signed-off-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>

--- plugins/operators/trim/src/trim/operator.py
+++ plugins/operators/trim/src/trim/operator.py
@@ -30,7 +30,7 @@ logger_trim = logging.getLogger(__name__)
                 Retrieves all measured entities from the entity source and samples the others following a certain order.
                 If the number of measured entity is too small, Trim instantiates a no-priors characterization operation.
                 """,
-    version="2.0.0",
+    version="2.0.3",
 )
 def trim(
     discoverySpace: DiscoverySpace = None,  # type: ignore[name-defined]

f32f7b476e30397614c2bbc85fb42cd8252315b8
Michele Gazzetti <michele.gazzetti1@ibm.com>
2026-07-13 16:17:50 +0100

    refactor(vllm-performance): rename experiment test-text-agent to test-agentic-tool-calling
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>

--- plugins/actuators/vllm_performance/ado_actuators/vllm_performance/actuator.py
+++ plugins/actuators/vllm_performance/ado_actuators/vllm_performance/actuator.py
@@ -256,7 +256,7 @@ class VLLMPerformanceTest(ActuatorBase):
             "test-geospatial-deployment-custom-dataset-v1",
             "test-geospatial-deployment-guidellm-v1",
             "test-geospatial-deployment-guidellm-custom-dataset-v1",
-            "test-text-agent",
+            "test-agentic-tool-calling",
         ]:
             if not self.env_manager:
                 raise MissingConfigurationForExperimentError(

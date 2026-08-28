8d04de7fcdcab4456f47e96daf83cbc2e60cbc62
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2026-08-28 11:58:17 +0100

    fix(trim): bump trim version to 2.1.0
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- plugins/operators/trim/src/trim/operator.py
+++ plugins/operators/trim/src/trim/operator.py
@@ -101,7 +101,7 @@ def validate_targetOutput(
                 Retrieves all measured entities from the entity source and samples the others following a certain order.
                 If the number of measured entity is too small, Trim instantiates a no-priors characterization operation.
                 """,
-    version="2.0.8",
+    version="2.1.0",
 )
 def trim(
     discoverySpace: DiscoverySpace = None,  # type: ignore[name-defined]

e80bc60d1183fd504f4361bbc8b4b401c2e61e65
danielelotito <danielelotito1@gmail.com>
2026-09-01 12:21:38 +0200

    fix(trim): bump version to 2.1.1

--- plugins/operators/trim/src/trim/operator.py
+++ plugins/operators/trim/src/trim/operator.py
@@ -101,7 +101,7 @@ def validate_targetOutput(
                 Retrieves all measured entities from the entity source and samples the others following a certain order.
                 If the number of measured entity is too small, Trim instantiates a no-priors characterization operation.
                 """,
-    version="2.1.0",
+    version="2.1.1",
 )
 def trim(
     discoverySpace: DiscoverySpace = None,  # type: ignore[name-defined]

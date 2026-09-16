536a6b6339522c0e01ee4566bf7bcc94c7df6a5a
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2026-09-16 09:06:09 +0100

    fix(trim): fix syntax error
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- plugins/operators/trim/src/trim/operator.py
+++ plugins/operators/trim/src/trim/operator.py
@@ -309,13 +309,13 @@ def trim(
     )
     sampler_params = TrimSamplerParametersInternal(
         **_trim_sampler_params.model_dump(),
-            noPriorsOperationId=(
+        noPriorsOperationId=(
             op_output_characterization_no_prior.operation.identifier
             if op_output_characterization_no_prior.operation is not None
             else None
         ),
     )
-    )
+
     trim_sampler_config = CustomSamplerConfiguration(
         module=trim_module, parameters=sampler_params
     )
